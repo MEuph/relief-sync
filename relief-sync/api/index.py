f690e9ca6b254033b353ec34e498bbd4549cf721
MEuph <christophermharris9999@gmail.com>
2025-05-03 17:14:52 -0500

    Forgot to change the default username and passowrd

--- relief-sync/api/index.py
+++ relief-sync/api/index.py
@@ -3,8 +3,8 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user='charris',
-    password='6035Euph_'
+    user='<username>',
+    password='<password>'
 )
 
 mycursor = mydb.cursor()
